14105f06caa6f5d3c248134070edd0aa4728bdff
vitukohdez <victorhdeztarano@gmail.com>
2026-02-24 23:51:04 +0100

    config of colors

---
+++ src/constants/Colors.ts
@@ -0,0 +1,22 @@
+const tintColorLight = "#2ABB9B"; // the green of the desgins, the const stablish one global const for the color to not have to change
+// all of them
+
+export default {
+  light: {
+    text: "#000",
+    background: "#FDFCF0", // fondo
+    tint: tintColorLight, // tint
+    tabIconDefault: "#ccc",
+    tabIconSelected: tintColorLight,
+  },
+  dark: {
+    text: "#fff",
+    background: "#000",
+    tint: tintColorLight,
+    tabIconDefault: "#ccc",
+    tabIconSelected: tintColorLight,
+  },
+
+  primary: "#2ABB9B",
+  secondary: "#FF7A5C",
+};
